098475a41581277df802f320ede7607fa7d575d8
Pradumna <pradumnabajoria@gmail.com>
2021-06-10 23:43:09 +0530

    learned basic require and copy file method

--- Node/index.js
+++ Node/index.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+//jshint esversion:6
+
+//console.log("hello world");
+
+// use node index.js in terminal to run code
+
+const fs = require("fs");
+
+fs.copyFileSync("file1.txt", "file2.txt");
